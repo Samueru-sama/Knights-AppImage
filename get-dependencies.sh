d1865096090e3f534474d24f39308c10d3c3d231
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-06-02 21:32:18 -0400

    mmm

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,7 +15,7 @@ pacman -Syu --noconfirm \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini kiconthemes-mini
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini kiconthemes-mini ! qt6-base
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
